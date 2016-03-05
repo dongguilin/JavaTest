01e02efb11e7d8de27ccfa941fa9bfc6177cfddf
dongguilin <guilin1_happy@163.com>
2016-03-05 08:21:34 +0800

    中断方法测试

---
+++ src/main/java/com/guilin/java6/thread/concurrent/canceled/InterruptTest.java
@@ -0,0 +1,21 @@
+package com.guilin.java6.thread.concurrent.canceled;
+
+/**
+ * Created by hadoop on 2016/2/28.
+ */
+public class InterruptTest {
+
+    public static void main(String[] args) {
+        System.out.println(Thread.currentThread().isInterrupted());//false
+
+        Thread.currentThread().interrupt();
+        System.out.println(Thread.currentThread().isInterrupted());//true
+
+        System.out.println(Thread.interrupted());//true,测试当前线程是否已经中断，并清除线程的中断状态
+        System.out.println(Thread.currentThread().isInterrupted());//false
+
+        Thread.currentThread().interrupt();
+        System.out.println(Thread.currentThread().isInterrupted());//true
+
+    }
+}
