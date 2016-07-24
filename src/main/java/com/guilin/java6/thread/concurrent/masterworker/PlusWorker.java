c02bfbd7c2eb48c31d27eb03e7364f67051a312d
dongguilin <guilin1_happy@163.com>
2016-07-24 09:33:38 +0800

    master/worker模式

---
+++ src/main/java/com/guilin/java6/thread/concurrent/masterworker/PlusWorker.java
@@ -0,0 +1,13 @@
+package com.guilin.java6.thread.concurrent.masterworker;
+
+/**
+ * Created by T57 on 2016/7/24 0024.
+ */
+public class PlusWorker extends Worker {
+
+    @Override
+    public Object handle(Object input) {
+        Integer i = (Integer) input;
+        return i * i * i;
+    }
+}
