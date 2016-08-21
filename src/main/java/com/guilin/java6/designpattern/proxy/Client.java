1f9b4876eca2660aa9b991c5ba8d9fe21f962af7
dongguilin <guilin1_happy@163.com>
2016-08-21 22:01:07 +0800

    静态代理

---
+++ src/main/java/com/guilin/java6/designpattern/proxy/Client.java
@@ -0,0 +1,12 @@
+package com.guilin.java6.designpattern.proxy;
+
+/**
+ * Created by T57 on 2016/8/21 0021.
+ */
+public class Client {
+
+    public static void main(String[] args) {
+        IDBQuery query = new DBQueryProxy();
+        query.request();
+    }
+}
---
+++ src/main/java/com/guilin/java6/designpattern/proxy/IDBQuery.java
@@ -0,0 +1,10 @@
+package com.guilin.java6.designpattern.proxy;
+
+/**
+ * Created by T57 on 2016/8/21 0021.
+ */
+public interface IDBQuery {
+
+    String request();
+
+}
