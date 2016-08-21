67b97825b4a6a2b1a13423e052be3f4831ea206d
dongguilin <guilin1_happy@163.com>
2016-08-21 19:02:26 +0800

    单例

---
+++ src/main/java/com/guilin/java6/designpattern/singleton/SerSingleton.java
@@ -0,0 +1,22 @@
+package com.guilin.java6.designpattern.singleton;
+
+import java.io.Serializable;
+
+/**
+ * Created by T57 on 2016/8/21 0021.
+ */
+public class SerSingleton implements Serializable {
+
+    private SerSingleton() {
+    }
+
+    private static SerSingleton instance = new SerSingleton();
+
+    public static SerSingleton getInstance() {
+        return instance;
+    }
+
+    private Object readResolve() {
+        return instance;//阻止生成新的实例，总是返回当前对象
+    }
+}
---
+++ src/main/java/com/guilin/java6/designpattern/singleton/StaticSingleton.java
@@ -0,0 +1,19 @@
+package com.guilin.java6.designpattern.singleton;
+
+/**
+ * Created by T57 on 2016/8/21 0021.
+ */
+public class StaticSingleton {
+
+    private StaticSingleton() {
+    }
+
+    //使用内部类来维护单例的实例
+    private static class SingletonHolder {
+        private static StaticSingleton instance = new StaticSingleton();
+    }
+
+    public static StaticSingleton getInstance() {
+        return SingletonHolder.instance;
+    }
+}
